e7f75cd3afa7ee8d97182363bac25ac4f6c4ba14
WahyuAtomic <mangoleho111@com>
2024-03-17 08:45:41 +0000

    +

---
+++ application/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+</head>
+<body>
+    <table id="example1" class="display">
+        <thead>
+            <tr>
+                <th>S.No</th>
+                <th>Name</th>
+                <th>Message</th>
+                <th>Age</th>
+            </tr>
+        </thead>
+        </table>  
+            <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+            <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function(){
+                    $('#example1').datatables();
+                });
+            </script>
+</body>
+</html>
